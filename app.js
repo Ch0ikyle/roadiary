03948ace3cbf0d10a8d20fe9b359e8e1c11e604b
최 화랑 <az8606@naver.com>
2018-12-26 07:14:01 +0000

    bodyParser, passport 설정 추가

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
-var express = require('express')
+var express = require('express');
+var mysql = require('mysql');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var fs = require("fs"); 
+var passport = requier('./config/passport.js');
+var dbconfig = require('./db.js');
 var app = express();
 
 var PORT = process.env.PORT || 3000;
+
+app.set('views', __dirname + '/views'); //html 파일의 위치 정의
+app.set('view engine', 'ejs'); // html 을 렌더링 할 때 ejs 엔진을 사용하도록 함.
+app.engine('html', require('ejs').renderFile);
+
+app.use(express.static('public'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(session({
+	secret : "#$#@KYLE@#$#", //쿠키 변조 방지 값
+	resave : false, // 
+	saveUninitialized : true
+}));
 
 var server = app.listen(PORT, function(){
     console.log("SERVER START..");
